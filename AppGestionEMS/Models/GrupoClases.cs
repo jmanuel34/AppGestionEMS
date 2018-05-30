0e77b57529c6176de352eac83cb7901df5407a46
jmanuel34 <jmanuel34@gmail.com>
2018-05-30 22:48:19 +0200

    Grupos de Clase

---
+++ AppGestionEMS/Models/GrupoClases.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppGestionEMS.Models
+{
+    public class GrupoClases
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public int cupo { get; set; } 
+    }
+}
